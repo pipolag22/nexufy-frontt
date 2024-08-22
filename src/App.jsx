ce06f69e9d3b1794e60c549f1a52a878613e0d11
GasparTorres <Gaspartorres352@gmail.com>
2024-08-22 17:37:16 -0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/home/Home";
-import Login from "./components/login/Login";
+import Login from "./components/login/LoginForm";
 import Register from "./components/register/Register";
 import Layout from "./components/home/Layout";
 import ProductItem from "./components/ProductPage/ProductItem";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const router = createBrowserRouter([
